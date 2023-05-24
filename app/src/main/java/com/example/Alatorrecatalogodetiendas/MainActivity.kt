e80398dcf7ceefc293091a42025243f3ef692133
lucia <lucia.alatorre8936@alumnos.udg.mx>
2023-05-24 02:06:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/Alatorrecatalogodetiendas/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.Alatorrecatalogodetiendas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.example.Alatorrecatalogodetiendas.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
